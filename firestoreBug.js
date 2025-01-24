bf29793da99ab1f90896c7fabf7afdfb439da19e
BugHunter-X <askzenai@gmail.com>
2025-01-24 07:22:06 +0100

    Add firestoreBug.js file

---
+++ firestoreBug.js
@@ -0,0 +1,1 @@
+In a Firebase project, I encountered an issue where data wasn't being written to Firestore despite seemingly correct code. The write operation didn't throw any errors, and the console logs showed the data being sent correctly. However, the data wasn't appearing in the Firestore database.
